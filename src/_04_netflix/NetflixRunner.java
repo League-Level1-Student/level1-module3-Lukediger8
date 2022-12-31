a7697c8fadcd1cd41b0c4b376be8e6e50fc47512
root <root@d5cbd7926b13>
2022-12-31 20:46:28 +0000

    e

--- src/_04_netflix/NetflixRunner.java
+++ src/_04_netflix/NetflixRunner.java
@@ -2,6 +2,34 @@ package _04_netflix;
 
 public class NetflixRunner {
 public static void main(String[] args) {
+	Movie one = new Movie("Naruto",5);
+	Movie two = new Movie("Demon Slayer",5);
+	Movie three = new Movie("Seven Deadly Sins",5);
+	Movie four = new Movie("Bleach", 5);
+	Movie five = new Movie("Home alone",5);
+	Movie six = new Movie("Home alone 2",5);
+	Movie seven = new Movie("Home alone 3",5);
+	Movie eight = new Movie("Home alone 4",5);
+	Movie nine =  new Movie("Home alone 5",5);
+	Movie ten = new Movie("Noelle",5);
 	
-}
-}
+	NetflixQueue queue = new NetflixQueue();
+	queue.addMovie(one);
+	queue.addMovie(two);
+	queue.addMovie(three);
+	queue.addMovie(four);
+	queue.addMovie(five);
+	queue.addMovie(six);
+	queue.addMovie(seven);
+	queue.addMovie(eight);
+	queue.addMovie(nine);
+	queue.addMovie(ten);
+	queue.printMovies();
+	queue.getBestMovie();
+
+	
+			
+		}
+	}
+
+
